7b897d9f0a2c102807878d0f1a255d6ba72cfcf4
Jacob Worck Jepsen <Jacob.worck@gmail.com>
2021-10-07 21:54:49 +0200

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ContentWidth = styled.div`
+  width: 200px;
+`;
+
+export interface Formvalues {
+  fruit: string;
+  car: string;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Center>
+        <h1>React common components</h1>
+        <ContentWidth>
+          {/*TODO: route to different components here*/}
+        </ContentWidth>
+      </Center>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
